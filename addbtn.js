495f30d24461525639cb3b5c386d84887757876a
cafferti <saulkumven@gmail.com>
2023-08-17 13:04:03 +0100

    completed multiple timeout function

--- addbtn.js
+++ addbtn.js
@@ -9,42 +9,45 @@ function runtwice(func) {
 
 runtwice(add);
 
-
-
 //setinterval
-count = 0
+count = 0;
 let looping = 0;
 let intervalid;
-const myfunction = function(){
-    btn = document.querySelector(`.js-btn`)
-    if(count === 0){
-        btn.innerHTML = `finished`
-        count = 1
-        
-    }else{
-        btn.innerHTML = `start`
-        count = 0
-        
-    }
-
-}
-
-
-
-const toggle = () =>{
-  if(looping === 0){
-
-    intervalid =   setInterval(myfunction, 1000)
-    
-    looping = 1
-
-  }else{
-    clearInterval(intervalid)
-    looping = 0
+const myfunction = function () {
+  btn = document.querySelector(`.js-btn`);
+  if (count === 0) {
+    btn.innerHTML = `finished`;
+    count = 1;
+  } else {
+    btn.innerHTML = `start`;
+    count = 0;
   }
- 
- 
-}
+};
+
+const toggle = () => {
+  if (looping === 0) {
+    intervalid = setInterval(myfunction, 1000);
+
+    looping = 1;
+  } else {
+    clearInterval(intervalid);
+    looping = 0;
+  }
+};
 //
 
+//setTimeout
 
+function toggled() {
+  btn = document.querySelector(`.js-b2tn`);
+  const secfunc = function () {
+    btn.innerHTML = `loading....`;
+  };
+
+  const secfuncfinal = function () {
+    btn.innerHTML = `finished!!`;
+  };
+
+  setTimeout(secfunc, 1000);
+  setTimeout(secfuncfinal, 3000);
+}
