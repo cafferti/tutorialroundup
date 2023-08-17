24cbc4c7040c52a25ea9ceb8927eb01bc5cc776f
cafferti <saulkumven@gmail.com>
2023-08-17 12:45:47 +0100

    start and finished btn interval

--- addbtn.js
+++ addbtn.js
@@ -9,16 +9,42 @@ function runtwice(func) {
 
 runtwice(add);
 
-count = 0;
-const toggle = () => {
-  setInterval(function () {
-    btn = document.querySelector(`.js-btn`);
-    if (count === 0) {
-      btn.innerHTML = `finished`;
-      count = 1;
-    } else {
-      btn.innerHTML = `start`;
-      count = 0;
+
+
+//setinterval
+count = 0
+let looping = 0;
+let intervalid;
+const myfunction = function(){
+    btn = document.querySelector(`.js-btn`)
+    if(count === 0){
+        btn.innerHTML = `finished`
+        count = 1
+        
+    }else{
+        btn.innerHTML = `start`
+        count = 0
+        
     }
-  }, 1000);
-};
+
+}
+
+
+
+const toggle = () =>{
+  if(looping === 0){
+
+    intervalid =   setInterval(myfunction, 1000)
+    
+    looping = 1
+
+  }else{
+    clearInterval(intervalid)
+    looping = 0
+  }
+ 
+ 
+}
+//
+
+
